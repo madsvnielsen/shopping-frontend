b5e3a184a0bdcf0caa962ae7387f5df07e595362
Magnus <magnusrk90@gmail.com>
2024-02-14 21:16:28 +0100

    Made the skeleton for the CheckoutCard

--- src/Components/CheckoutCard.tsx
+++ src/Components/CheckoutCard.tsx
@@ -1,11 +1,52 @@
 import './CheckoutCard.css'
-import reactLogo from "../assets/lowrescard.png";
+import cardImage from "../assets/lowrescard.png";
+import {useState} from "react";
 
 
 function CheckoutCard(){
+
+    const unitPrice = 125;
+    const [amount, setAmount] = useState(1);
+
+    const handleChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        setAmount(Number(event.target.value));
+    };
+
     return (
         <div className="box">
-            <img src={reactLogo} className="logo react" alt="React logo" />
+            <img src={cardImage} className="pokemonImg" alt="Pokemon card"/>
+            <div className="textBox">
+                <p className="headline">
+                    Name
+                </p>
+                <p>
+                    Venusaur
+                </p>
+                <p className="headline">
+                    Set
+                </p>
+                <p>
+                    XY
+                </p>
+                <p className="headline">
+                    Rarity
+                </p>
+                <p>
+                    Rare Holo EX
+                </p>
+            </div>
+            <div className="textBox">
+                <select name="amount" id="cards" onChange={handleChange}>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                </select>
+                <p>
+                    ${unitPrice * amount}
+                </p>
+            </div>
         </div>
     )
 }
