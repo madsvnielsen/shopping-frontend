01834c9261ca19b013bd2fcd8e798f7e451dbbe1
Magnus <magnusrk90@gmail.com>
2024-02-14 14:21:34 +0100

    Started working on the card with items in the basket

--- src/Components/CheckoutCard.tsx
+++ src/Components/CheckoutCard.tsx
@@ -5,9 +5,7 @@ import reactLogo from "../assets/lowrescard.png";
 function CheckoutCard(){
     return (
         <div className="box">
-            <a href="https://react.dev" target="_blank">
-                <img src={reactLogo} className="logo react" alt="React logo" />
-            </a>
+            <img src={reactLogo} className="logo react" alt="React logo" />
         </div>
     )
 }
