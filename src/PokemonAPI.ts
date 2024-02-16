52c7b3d474ca81d9576f0e49143f51a5b7de3f75
Magnus <magnusrk90@gmail.com>
2024-02-16 10:47:10 +0100

    Made frontend access getCard endpoint

---
+++ src/PokemonAPI.ts
@@ -0,0 +1,23 @@
+import {Card} from "./interfaces/Card.tsx";
+
+export class PokemonAPI {
+    static async getCard(id: string): Promise<Card> {
+        return fetch('https://api.pokemontcg.io/v2/cards/'+id,
+            {
+                method: 'GET',
+                headers: {
+                    Accept: 'application/json',
+                    'Content-Type': 'application/json',
+                    Authorization: 'Bearer ' + "e7c3a10b-7fc1-4ddc-a225-f3412514f740",
+                }
+            }
+        )
+            .then(response => response.json())
+            .then(data => {
+                return data.data;
+            })
+            .catch(error => {
+                Promise.reject(error);
+            });
+    }
+}
