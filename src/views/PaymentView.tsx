27fa30682ec6357e67648274c7318a755d6ce8f1
asbp <s194582@student.dtu.dk>
2024-03-11 15:06:10 +0100

    Added payment option to PaymentView

--- src/views/PaymentView.tsx
+++ src/views/PaymentView.tsx
@@ -5,13 +5,15 @@ import React, { useState } from 'react';
 
 export function PaymentView() {
 
+    const [isChecked, setIsChecked] = useState();
+    function handlePaymentChoice() {}
 
     
     return (
         <div>
             <Banner/>
             <StatusBar /> {}
-            <form action="/my-handling-form-page" method="post">
+            <form action="handle_submit_form" method="post">
                 <fieldset>
                     <legend>Delivery address</legend>
                     <div>
@@ -23,28 +25,28 @@ export function PaymentView() {
                         </p>
                         <p>
                             <label htmlFor="name">Full name </label>
-                            <input type="text" id="name" name="user_name" maxLength={65} />
-                        </p>
-                        <p>
-                            <label htmlFor="address1">Street address </label>
-                            <input type="text" id="address1" name="user_address1" maxLength={65} />
-                            <input type="text" id="address2" name="user_address2" maxLength={65} />
-                        </p>
-                        <p>
-                            <label htmlFor="zip">Postal code </label>
-                            <input type="" id="zip" name="user_zip" pattern="[1-9]{1}[0-9]{3}" /*TODO: match against https://api.dataforsyningen.dk/postnumre*//>
-                        </p>
-                        <p>
-                            <label htmlFor="city">City </label>
-                            <input type="text" id="city" name="user_city" /*TODO: Provide automatically from zip*/ /> 
+                            <input type="text" id="name" name="user_name" required maxLength={65} />
                         </p>
                         <p>
                             <label htmlFor="comp_name">Company name </label>
                             <input type="text" id="comp_name" name="user_company" maxLength={65} />
                         </p>
                         <p>
-                            <label htmlFor="comp_vat">Company VAT VALIDATE </label>
-                            <input type="" id="comp_vat" name="user_companyvat" required maxLength={65}/>
+                            <label htmlFor="comp_vat">Company VAT</label>
+                            <input type="" id="comp_vat" name="user_companyvat" required maxLength={65} /*TODO: validate VAT*//>
+                        </p>
+                        <p>
+                            <label htmlFor="address1">Street address </label>
+                            <input type="text" id="address1" name="user_address1" required maxLength={65} />
+                            <input type="text" id="address2" name="user_address2" maxLength={65} />
+                        </p>
+                        <p>
+                            <label htmlFor="zip">Postal code </label>
+                            <input type="number" id="zip" name="user_zip" pattern="[1-9]{1}[0-9]{3}" required /*TODO: match against https://api.dataforsyningen.dk/postnumre*//>
+                        </p>
+                        <p>
+                            <label htmlFor="city">City </label>
+                            <input type="text" id="city" name="user_city" /*TODO: Provide automatically from zip*/ /> 
                         </p>
                         <p>
                             <label htmlFor="phone">Phone </label>
@@ -52,8 +54,19 @@ export function PaymentView() {
                         </p>
                         <p>
                             <label htmlFor="mail">Email </label>
-                            <input type="email" id="mail" name="user_email" pattern="^[\w-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,3}$"/>
+                            <input type="email" id="mail" name="user_email" pattern="^[\w-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,3}$" required />
                         </p>
+                        Is delivery address and billing address the same?
+                        <ul>
+                            <li>
+                                <label htmlFor="same_address">Yes</label>
+                                <input type="radio" id="same_address" name="address" value="same_address" checked={true} />
+                            </li>
+                            <li>
+                                <label htmlFor="notsame_address">No</label>
+                                <input type="radio" id="notsame_address" name="adress" value="notsame_address" />
+                            </li>
+                        </ul>
                     </div>
                 </fieldset>
 
@@ -63,17 +76,30 @@ export function PaymentView() {
                         <ul>
                             <li>
                                 <label htmlFor="credit_card">Credit card</label>
-                                <input type="radio" id="credit_card" name="payment_option" value="credit_card" checked />
+                                <input type="radio" id="credit_card" name="payment_option" value="credit_card" checked={true} 
+                                    onChange={handlePaymentChoice}/>
                             </li>
                             <li>
-                                <label htmlFor="mobile_pay">MobilePay</label>
-                                <input type="radio" id="mobile_pay" name="payment_option" value="mobile_pay" />
+                                <label htmlFor="pokemon_pay">Pokemon Trade</label>
+                                <input type="radio" id="pokemon_pay" name="payment_option" value="pokemon_pay" />
                             </li>
                             <li>
-                                <label htmlFor="google_pay">GooglePay</label>
-                                <input type="radio" id="google_pay" name="payment_option" value="google_pay" />
+                                <label htmlFor="kidney_pay">Kidney Payment</label>
+                                <input type="radio" id="kidney_pay" name="payment_option" value="kidney_pay" />
                             </li>
                         </ul>
+                        <p>
+                            <label htmlFor="card_no">Card Number </label>
+                            <input type="number" id="card_no" name="user_card" required maxLength={16} />
+                        </p>
+                        <p>
+                            <label htmlFor="exp_date">Exp. date </label>
+                            <input type="number" id="exp_date" name="user_expdate" required maxLength={8} />
+                        </p>
+                        <p>
+                            <label htmlFor="card_cvv">CVV </label>
+                            <input type="number" id="card_cvv" name="user_cardcvv" required maxLength={3} />
+                        </p>
                     </div>
                 </fieldset>
 
