ceb8a1466438728acffc37c522c3ef10d13359bc
MathiasW <s224293@student.dtu.dk>
2024-03-11 16:07:47 +0100

    Test for QuantDiscount

---
+++ src/Tests/QuantDiscount.test.tsx
@@ -0,0 +1,38 @@
+import { render, screen, waitFor} from "@testing-library/react";
+import { describe, expect, it } from "vitest";
+import {BasketOverview} from "../views/BasketOverview.tsx";
+import {BasketItem, DetailedBasketItem} from "../interfaces/BasketItem.ts";
+import {Card} from "../interfaces/Card.tsx";
+import {PokemonAPI} from "../PokemonAPI.ts";
+import quantDiscount from "../HelperFunction/QuantDiscount.ts";
+
+
+
+const basketMockSingleCard : BasketItem[] = [{
+    id: "base1-3",
+    quantity : 4,
+    isLaminated: false
+}
+]
+describe(quantDiscount.name, () => {
+
+
+  it("Should give discount", async() =>{
+      const basketItems : DetailedBasketItem[] = []
+      for await (const item of basketMockSingleCard) {
+          const card : Card = await PokemonAPI.getCard(item.id)
+          basketItems.push({
+              id: item.id,
+              card: card,
+              quantity: item.quantity,
+              isLaminated: item.isLaminated
+          } as DetailedBasketItem)
+      }
+      render(<BasketOverview basketMock={basketMockSingleCard} />);
+
+      await waitFor(() => expect(screen.getByText(basketItems[0].card.name)).toBeInTheDocument(), { timeout: 5000 });
+      expect(screen.queryByText("-")).toBeNull();
+
+    } )
+
+})
