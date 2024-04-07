5866a1ed64e01e85f6e0a12635dd8085b433988f
asbp <s194582@student.dtu.dk>
2024-04-07 20:30:58 +0200

    MobilePay 8 digit validation added.

--- src/Components/PhoneDetails.tsx
+++ src/Components/PhoneDetails.tsx
@@ -4,7 +4,7 @@ function PhoneDetails(){
     return(
         <div className="box">
             <label htmlFor="phone_no">Phone Number</label>
-            <input className="form-control" type="text" id="phone_no" name="user_phone_no"/>
+            <input className="form-control" type="text" id="phone_no" name="user_phone_no" pattern="[1-9]{1}[0-9]{7}"/>
         </div>
     )
 }
