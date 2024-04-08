26d89c66d08ddee3f733a7c58ccaadfc928650ec
MathiasW <s224293@student.dtu.dk>
2024-04-08 16:54:58 +0200

    Merged placer order into main

--- src/Components/PlaceOrder.tsx
+++ src/Components/PlaceOrder.tsx
@@ -1,16 +1,8 @@
 import PlaceOrderFunc from "../HelperFunction/PlaceOrderFunc.tsx";
-import './PlacerOrder.css';
-import './CheckBox.tsx'
-import Checkbox from "./CheckBox.tsx";
+import './PlaceOrder.css';
 function PlaceOrder(){
-
-
     return(
         <div className="placerOrderBox">
-            /*
-            <Checkbox label="I agree to the the Terms of service"/>
-            <Checkbox label="I agree to recieve news and marketing emails"/>
-            */
             <button onClick={() => PlaceOrderFunc("placeHolder", "Placeholder")} className="placeOrderButton">
                 Place Order
             </button>
