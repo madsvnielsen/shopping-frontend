423f3197ad56d022f09d0e01dc70eb2e1604dbb2
Magnus <magnusrk90@gmail.com>
2024-04-19 10:47:56 +0200

    started working on getting the basket

--- src/views/BasketOverview.tsx
+++ src/views/BasketOverview.tsx
@@ -1,4 +1,3 @@
-
 import {useEffect, useState} from "react";
 import ShoppingCart from "./../Components/ShoppingCart.tsx";
 import {BasketItem, DetailedBasketItem} from "./../interfaces/BasketItem";
@@ -9,7 +8,7 @@ import StatusBar from "./../Components/StatusBar";
 import DiscountBanner from "./../Components/DiscountBanner";
 import RecommendedProducts from "./../Components/RecommendedProducts";
 
-const recommendedMock : string[] = [
+const recommendedMock: string[] = [
     "base2-4",
     "xy1-4",
     "xy2-5",
@@ -19,22 +18,21 @@ const recommendedMock : string[] = [
 ]
 
 
-export function BasketOverview(props : { basket : BasketItem[]}) {
+export function BasketOverview() {
     const [basketItems, setBasketItems] = useState([] as DetailedBasketItem[])
-    const [isLoading, setIsLoading] = useState(true)
+    const [isLoading] = useState(true)
     const [recommendedItems, setRecommendedItems] = useState([] as Card[])
-    
-    
 
-    const updateBasketItem = (basketItem : DetailedBasketItem) =>  {
+
+    const updateBasketItem = (basketItem: DetailedBasketItem) => {
 
         const index = basketItems.findIndex(item => item.id === basketItem.id);
-        const newBasketItems = [... basketItems];
+        const newBasketItems = [...basketItems];
 
         //Change item if quantity is positive, otherwise remove item.
-        if(basketItem.quantity >= 0 || isNaN(basketItem.quantity)){
+        if (basketItem.quantity >= 0 || isNaN(basketItem.quantity)) {
             newBasketItems[index] = basketItem;
-        } else{
+        } else {
             newBasketItems.splice(index, 1);
         }
         setBasketItems(newBasketItems)
@@ -42,67 +40,73 @@ export function BasketOverview(props : { basket : BasketItem[]}) {
 
 
     const addBasketItem = async (card: Card) => {
-        /*
-        const newItem : DetailedBasketItem = {
+
+        const newItem: DetailedBasketItem = {
             id: card.id,
             card: card,
             quantity: 1,
             isLaminated: false
         }
 
-         */
-        PokemonAPI.addToBasket(card.id, "1")
-        const updatedBasket = await PokemonAPI.getBasket();
-        setBasketItems(updatedBasket);
 
-        // setBasketItems([...basketItems, newItem]);
+        PokemonAPI.addToBasket(card.id, "1")
+        setBasketItems([...basketItems, newItem]);
     }
 
     useEffect(() => {
-        async function loadBasketItems(){
-            const newBasketItems : DetailedBasketItem[] = []
-            for await (const item of props.basket) {
-                const card : Card = await PokemonAPI.getCard(item.id)
-                newBasketItems.push({
-                    id: item.id,
-                    card: card,
-                    quantity: item.quantity,
-                    isLaminated: item.isLaminated
-                } as DetailedBasketItem)
-            }
-            setIsLoading(false)
-            setBasketItems(newBasketItems)
-        }
+            async function loadBasketItems() {
+                try {
+                    const basketItems: BasketItem[] = await PokemonAPI.getBasket();
 
-        async function loadRecommendedProducts(){
-            const newRecommendedItems  : Card[] = []
-            for await (const id of recommendedMock){
-                const card : Card = await PokemonAPI.getCard(id);
-                newRecommendedItems.push(card);
+                    const detailedBasketItems: DetailedBasketItem[] = await Promise.all(
+                        basketItems.map(async (item) => ({
+                            id: item.id,
+                            quantity: item.quantity,
+                            isLaminated: item.isLaminated,
+                            card: await PokemonAPI.getCard(item.id),
+                        }))
+                    );
+
+                    setBasketItems(detailedBasketItems);
+                } catch (error) {
+                    console.error("Error loading basket items:", error);
+                }
             }
 
-            setRecommendedItems(newRecommendedItems);
+            async function loadRecommendedProducts() {
+                const newRecommendedItems: Card[] = []
+                for await (const id of recommendedMock) {
+                    const card: Card = await PokemonAPI.getCard(id);
+                    newRecommendedItems.push(card);
+                }
+
+                setRecommendedItems(newRecommendedItems);
+
+            }
+
+            loadBasketItems()
+            loadRecommendedProducts()
 
         }
 
-        loadBasketItems()
-        loadRecommendedProducts()
+        ,
+        []
+    )
 
-    }, [])
-
-    const nothingToDisplayText = isLoading ? <h1 className="loading-text">Loading...</h1> : <h1>Your shopping cart is empty!</h1>
+    const nothingToDisplayText = isLoading ? <h1 className="loading-text">Loading...</h1> :
+        <h1>Your shopping cart is empty!</h1>
 
 
     return (
         <div>
             <Banner goToBasket={false}/>
             <StatusBar activeStep={1}/>
-            <DiscountBanner />
-            {basketItems.length > 0 && <ShoppingCart basketItems={basketItems} updateBasketItem={updateBasketItem} />}
+            <DiscountBanner/>
+            {basketItems.length > 0 && <ShoppingCart basketItems={basketItems} updateBasketItem={updateBasketItem}/>}
             {basketItems.length === 0 && nothingToDisplayText}
             <RecommendedProducts
-            productsList={recommendedItems}
-            onProductAdded={addBasketItem}
+                productsList={recommendedItems}
+                onProductAdded={addBasketItem}
             />
         </div>
     );
