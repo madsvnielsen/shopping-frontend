f57df416c49d01932bf2a3a4d0ef741172594b47
Ismail A. <s190201@student.dtu.dk>
2024-04-29 17:09:28 +0200

    Added placeholder to all boxes

--- src/Components/InvoiceDetails.tsx
+++ src/Components/InvoiceDetails.tsx
@@ -1,15 +1,30 @@
-import './CardDetails.css';
+import "./CardDetails.css";
 
-function InvoiceDetails(){
-    return(
-        <div className="box">
-            <label htmlFor="comp_vat">Company name</label>
-            <input className="form-control" type="text" id="comp_name" name="user_company" required maxLength={65}/>
-            <label htmlFor="comp_vat">Company VAT</label>
-            <input className="form-control" type="number" id="comp_vat" name="user_companyvat"
-                   required pattern="[0-9]{8}"/>
-        </div>
-    )
+function InvoiceDetails() {
+  return (
+    <div className="box">
+      <label htmlFor="comp_vat">Company name</label>
+      <input
+        className="form-control"
+        type="text"
+        id="comp_name"
+        name="user_company"
+        required
+        maxLength={65}
+        placeholder="Enter company name"
+      />
+      <label htmlFor="comp_vat">Company VAT</label>
+      <input
+        className="form-control"
+        type="number"
+        id="comp_vat"
+        name="user_companyvat"
+        required
+        pattern="[0-9]{8}"
+        placeholder="Enter company VAT"
+      />
+    </div>
+  );
 }
 
-export default InvoiceDetails
+export default InvoiceDetails;
